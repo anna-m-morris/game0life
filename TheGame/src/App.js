d0a9e4cc6e5523dae8050741fe519227a0c39a65
Anna Morris <annapatton444@gmail.com>
2020-09-21 19:26:18 -0500

    restarting my computer

--- TheGame/src/App.js
+++ TheGame/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Board from "./Game/Board"
+import Board from "./Game/Game"
 
 function App() {
   return (
---
+++ TheGame/src/Game/Game.js
@@ -0,0 +1,17 @@
+import React, { useState, useCallback, useRef } from 'react';
+
+const xAxis = 25 //rows
+const yAxis = 25 //collumns
+
+const Game = () => {
+    const [board, setBoard] = useState()
+    const newGame = () => {
+
+    }
+    setBoard(newGame)
+    return (
+        <div>Here we go again</div>
+    )
+}
+
+export default Game
