21cc87500a2256de002c889d4b1077c78410e92b
Anna Morris <annapatton444@gmail.com>
2020-09-21 20:28:51 -0500

    cells are on state and linked to neighbors

--- TheGame/src/Game/Game.js
+++ TheGame/src/Game/Game.js
@@ -3,12 +3,36 @@ import React, { useState, useCallback, useRef } from 'react';
 const xAxis = 25 //rows
 const yAxis = 25 //collumns
 
-const Game = () => {
-    const [board, setBoard] = useState()
-    const newGame = () => {
+class Cell {
+    constructor(x, y) {
+        this.location = [x, y]
+        this.neighbors = {
+            north: [x + 1, y],
+            northEast: [x + 1, y + 1],
+            east: [x, y + 1],
+            southEast: [x - 1, y + 1],
+            south: [x - 1, y],
+            southWest: [x - 1, y - 1],
+            west: [x, y - 1],
+            northWest: [x + 1, y - 1]
+        }
+        this.isAlive = false
 
     }
-    setBoard(newGame)
+}
+
+const Game = () => {
+    const [board, setBoard] = useState(() => {
+        const cells = []
+        for (let x = 0; x < xAxis; x++) {
+            for (let y = 0; y < yAxis; y++) {
+                cells.push(new Cell(x,y))
+            }
+        }
+        return (cells)
+    })
+
+    console.log(board)
     return (
         <div>Here we go again</div>
     )
