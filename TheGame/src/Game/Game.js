338f82a4f3dfa32809eb2d3bb7eec7f6e8fea488
Anna Morris <annapatton444@gmail.com>
2020-09-22 20:30:38 -0500

    working on functionality

--- TheGame/src/Game/Game.js
+++ TheGame/src/Game/Game.js
@@ -7,16 +7,16 @@ const yAxis = 25 //collumns
 class Cell {
     constructor(x, y) {
         this.location = [x, y]
-        this.neighbors = {
-            north: [x + 1, y],
-            northEast: [x + 1, y + 1],
-            east: [x, y + 1],
-            southEast: [x - 1, y + 1],
-            south: [x - 1, y],
-            southWest: [x - 1, y - 1],
-            west: [x, y - 1],
-            northWest: [x + 1, y - 1]
-        }
+        this.neighbors = [
+            this.north = [x + 1, y],
+            this.northEast = [x + 1, y + 1],
+            this.east = [x, y + 1],
+            this.southEast = [x - 1, y + 1],
+            this.south = [x - 1, y],
+            this.southWest = [x - 1, y - 1],
+            this.west = [x, y - 1],
+            this.northWest = [x + 1, y - 1]
+        ]
         this.isAlive = false
     }
 }
@@ -32,23 +32,56 @@ const Game = () => {
         return (cells)
     })
 
+    const findByLocation = (board, location) => {
+        let result = {}
+        board.forEach(cell => {
+            if (location[0] === cell.location[0] && location[1] === cell.location[1]) {
+                result = cell
+            }
+        })
+        return result
+    }
+
+
     console.log(board)
-    return (
+    return (<div>
         <div className="board">{board.map(cell => {
             return (<CellDiv
                 key={cell.location}
-                cell={cell} />)
-        })}</div>
+                cell={cell}
+                board={board} />)
+        })}
+        </div>
+        <button
+            onClick={() => {
+                let newBoard = board.map((cell) => {
+                    let liveOnes = 0
+                    cell.neighbors.forEach((neighbor) => {
+                        let current = findByLocation(board, neighbor)
+                        if (current.isAlive === true) {
+                            liveOnes++
+                            console.log(liveOnes)
+                        }
+                    })
+                    if(liveOnes===2||liveOnes===3){cell.isAlive=true}
+                    else if (liveOnes<2||liveOnes>3){cell.isAlive=false}
+                    return cell
+                })
+                setBoard(newBoard)
+            }}>Run</button>
+        <button>Stop</button>
+    </div>
     )
 }
 
 const CellDiv = (props) => {
     const [state, setState] = useState(false)
+    props.cell.isAlive=state
     return (
         <div className={state ? "alive" : "dead"}
             onClick={() => {
-                props.cell.isAlive = !props.cell.isAlive
                 setState(!state)
+                console.log(props.cell)
             }}></div>
     )
 }
