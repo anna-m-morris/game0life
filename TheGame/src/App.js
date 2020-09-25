7c4b22dd35ec6de3e98d97226235e4b4506381bf
Anna Morris <annapatton444@gmail.com>
2020-09-24 22:23:08 -0500

    info page

--- TheGame/src/App.js
+++ TheGame/src/App.js
@@ -4,9 +4,10 @@ import Board from "./Game/Game"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"><div>
       Conway's (Anna's) Game of Life.
-      <Board></Board>
+      <Board></Board></div>
+      <div>If a cell has 3 live neighbors it comes to life<br/>If a cell has 2 or 3 live neigbors and was already alive it stays alive<br/>If a cell has less than two neighbors or more than four it dies.</div>
     </div>
   );
 }
